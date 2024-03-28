e42edfaade765000a7355042a5114be83d231bf1
dv-secure <164401256+dv-secure@users.noreply.github.com>
2024-03-28 17:49:37 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -15,23 +15,54 @@ function showPosition(position) {
   x.innerHTML =
     "PGP Key: " + pgp + position.coords.latitude +
     "<br>SHA-256 Key: " + shaKey + position.coords.longitude;
-  localStorage.setItem('myList', JSON.stringify(x.innerHTML))
+  localStorage.setItem('myList', JSON.stringify(x.innerHTML));
+  // console.log(x)
 }
 
+let logData = localStorage.getItem('myList');
+
+console.log(`Updated Logo Data ${logData}`)
+
+
+// console.log(x)
 
 const retryButton = document.querySelector('.js-retry-location');
 retryButton.addEventListener('click', event => {
   navigator.geolocation.getCurrentPosition((coords) => {
-    console.log('coords');
+    // console.log('coords');
   });
 });
 
-
 const clickButton = document.getElementById('sendBtn');
-
 clickButton.addEventListener("click", () => {
-  console.log("Clicked")
+  // console.log("Clicked")
   clickButton.style.display = "none"
 })
+
+
+window.onload = function () {
+  var button = document.getElementById('sendBtn');
+  setInterval(function () {
+    button.click();
+  }, 1000);  // this will make it click again every 1000 miliseconds
+};
+
+// document.getElementById("demo").onblur = function () {
+//   var userText = this.value;
+// }
+
+// var savedInfo = document.getElementById('demo').value
+
+
+// function getname(name) {
+//   name = document.getElementById("generatedData").value;
+//   return name;
+// }
+
+console.log(x)
+
+
+
+
 
 
